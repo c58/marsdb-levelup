e141e0155c87c808908a41ccb922829314fad4f7
Artem Artemyev <viiv.c58@gmail.com>
2016-02-13 20:49:41 +0500

    0.1.0

--- dist/LevelStorageManager.js
+++ dist/LevelStorageManager.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
-
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _marsdb = require('marsdb');
 
@@ -32,7 +32,7 @@ var _stores = {};
  * LevelUP storage implementation.
  */
 
-var LevelStorageManager = (function (_StorageManager) {
+var LevelStorageManager = function (_StorageManager) {
   _inherits(LevelStorageManager, _StorageManager);
 
   function LevelStorageManager(db) {
@@ -156,6 +156,6 @@ var LevelStorageManager = (function (_StorageManager) {
   }]);
 
   return LevelStorageManager;
-})(_marsdb.StorageManager);
+}(_marsdb.StorageManager);
 
 exports.default = LevelStorageManager;
